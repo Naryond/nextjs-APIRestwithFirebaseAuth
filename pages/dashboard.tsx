01fa96dfcde41dde86b4f38256ac957b0f131f9e
Naryond <giuggey85@gmail.com>
2022-10-11 15:54:50 +0100

    .

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Dashboard = () => {
-  return <div>This route is protected</div>;
-};
+interface data {
+  preventDefault: any;
+}
 
-export default Dashboard;
+export default function Dashboard() {
+  const [input, setInput] = useState('');
+  const [user, setUser] = useState({});
+  const [picture, setPicture] = useState('');
+
+  const getUser = (e: any) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e: data) => {
+    e.preventDefault();
+    fetch(`https://api.github.com/users/${input}`).then((response) =>
+      response.json().then((data) => {
+        setUser(data);
+        setPicture(`${data.avatar_url}`);
+      })
+    );
+  };
+
+  return (
+    <div className="box">
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={getUser}></input>
+        <button type="submit">Insert</button>
+      </form>
+      <br />
+      <div>{user.name}</div>
+      <div>{user.location}</div>
+      <div>{user.company}</div>
+      <div>{user.blog}</div>
+      <img src={picture} alt="" />
+    </div>
+  );
+}
--- pages/login.tsx
+++ pages/login.tsx
@@ -19,7 +19,6 @@ const Login = () => {
     } catch (err) {
       console.log(err);
     }
-    console.log(data);
   };
 
   return (
