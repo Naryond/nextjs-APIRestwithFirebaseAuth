e412df52be071c848c3e48d8d060893fb5498ed1
Naryond <giuggey85@gmail.com>
2022-10-14 19:44:33 +0100

    .

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,11 +1,60 @@
 import React, { useState } from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
 interface data {
   preventDefault: any;
   target: any;
 }
 
-export default function Dashboard() {
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const labels = ['2017', '2018', '2019', '2020', '2021', '2022'];
+
+const options = {};
+
+const info = {
+  labels,
+  datasets: [
+    {
+      label: 'React',
+      data: [32, 42, 51, 60, 51, 95],
+      backgroundColor: '#2196F3',
+      borderColor: '#2196F3',
+    },
+    {
+      label: 'Angular',
+      data: [37, 42, 41, 37, 31, 44],
+      backgroundColor: '#F44236',
+      borderColor: '#F44236',
+    },
+    {
+      label: 'Vue',
+      data: [60, 54, 54, 28, 27, 49],
+      backgroundColor: '#FFCA29',
+      borderColor: '#FFCA29',
+    },
+  ],
+};
+
+const Dashboard = () => {
   const [input, setInput] = useState('');
   const [user, setUser] = useState<any>({});
   const [picture, setPicture] = useState('');
@@ -26,23 +75,34 @@ export default function Dashboard() {
   };
 
   return (
-    <div className="mb-3 p-5">
-      <form onSubmit={handleSubmit}>
-        <input type="text" onChange={getUser} placeholder="Insert User"></input>
-        <button type="submit">Search</button>
-      </form>
-      <br />
-      {user.login && (
-        <div>
-          <div>Name: {user.name}</div>
-          <div>Location: {user.location}</div>
-          <div>Company: {user.company}</div>
-          <div>Web Page: {user.blog}</div>
-          <picture>
-            <img src={picture} alt="" />
-          </picture>
-        </div>
-      )}
-    </div>
+    <>
+      <div className="mb-3 p-5">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            onChange={getUser}
+            placeholder="Insert User"
+          ></input>
+          <button type="submit">Search</button>
+        </form>
+        <br />
+        {user.login && (
+          <div>
+            <div>Name: {user.name}</div>
+            <div>Location: {user.location}</div>
+            <div>Company: {user.company}</div>
+            <div>Web Page: {user.blog}</div>
+            <picture>
+              <img src={picture} alt="" />
+            </picture>
+          </div>
+        )}
+      </div>
+      <div style={{ width: 600, height: 300 }}>
+        <Line options={options} data={info} />
+      </div>
+    </>
   );
-}
+};
+
+export default Dashboard;
