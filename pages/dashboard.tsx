7bf0581e754065b4d007851dc2cecb635eb8f6aa
Naryond <giuggey85@gmail.com>
2022-10-16 22:01:52 +0100

    .

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,34 +1,7 @@
 import React, { useState } from 'react';
 import User from './user';
 import { useForm } from 'react-hook-form';
-
-const labels = ['2017', '2018', '2019', '2020', '2021', '2022'];
-
-const options = {};
-
-const info = {
-  labels,
-  datasets: [
-    {
-      label: 'React',
-      data: [32, 42, 51, 60, 51, 95],
-      backgroundColor: '#2196F3',
-      borderColor: '#2196F3',
-    },
-    {
-      label: 'Angular',
-      data: [37, 42, 41, 37, 31, 44],
-      backgroundColor: '#F44236',
-      borderColor: '#F44236',
-    },
-    {
-      label: 'Vue',
-      data: [60, 54, 54, 28, 27, 49],
-      backgroundColor: '#FFCA29',
-      borderColor: '#FFCA29',
-    },
-  ],
-};
+import { options, info } from './chartData';
 
 const Dashboard = () => {
   const [user, setUser] = useState<any>({});
