76234b63edd6852d3c023afd1d1ab9782c387f25
Naryond <giuggey85@gmail.com>
2022-11-25 16:57:15 +0000

    .

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -4,6 +4,7 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 import { info } from '../components/chartData';
 import { Container, Row, Col, Button, Form } from 'react-bootstrap';
 import Settings from '../components/settings';
+import { getAuth } from 'firebase/auth';
 
 export type Fetched = {
   login: string;
@@ -26,6 +27,10 @@ type Dataset = {
 };
 
 export type info = { labels: string; datasets: Dataset };
+
+const userGuest = 'guest@access.com';
+const userEmail: any = getAuth().currentUser?.email;
+console.log(userEmail);
 
 const Dashboard = () => {
   const [user, setUser] = useState<Fetched>();
@@ -62,12 +67,14 @@ const Dashboard = () => {
             {user?.login && <Profile {...user} />}
           </div>
         </Col>
-        <Col>
-          <div className="mb-3 p-5">
-            <Button onClick={editMode}>Settings</Button>
-            {edit ? <Settings closePage={editMode} /> : null}
-          </div>
-        </Col>
+        {userEmail !== userGuest ? (
+          <Col>
+            <div className="mb-3 p-5">
+              <Button onClick={editMode}>Settings</Button>
+              {edit ? <Settings closePage={editMode} /> : null}
+            </div>
+          </Col>
+        ) : null}
       </Row>
     </Container>
   );
