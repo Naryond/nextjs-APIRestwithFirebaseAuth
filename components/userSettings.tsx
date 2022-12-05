970dde69b673ce4f07f5fc55b138ce09008d0474
Naryond <giuggey85@gmail.com>
2022-12-05 18:09:34 +0000

    .

--- components/userSettings.tsx
+++ components/userSettings.tsx
@@ -2,22 +2,20 @@ import React from 'react';
 import { Container, Button } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
 import { updatePassword } from 'firebase/auth';
-// import { useAuth } from '../context/AuthContext';
 import { auth } from '../config/firebase';
 
 const UserSettings = () => {
   const { register, handleSubmit, reset } = useForm();
 
-  // const { user } = useAuth();
   const user: any = auth.currentUser;
-
-  // Trying to verify the current password first
 
   const onSubmit = async ({ password, confirmPassword }: any) => {
     try {
-      if (password === confirmPassword) await updatePassword(user, password);
-      reset();
-      alert('credentials updated');
+      if (password === confirmPassword) {
+        await updatePassword(user, password);
+        reset();
+        alert('credentials updated');
+      }
     } catch (err) {
       console.log(err);
       alert(err);
@@ -28,14 +26,6 @@ const UserSettings = () => {
     <Container className="mt-3">
       <h5>Update Profile</h5>
       <form onSubmit={handleSubmit(onSubmit)}>
-        {/* <div className="m-3">
-          <label className="mx-2">Current Password</label>
-          <input
-            {...register('oldPassword')}
-            type="password"
-            placeholder="Enter current password"
-          />
-        </div> */}
         <div className="m-3">
           <label className="mx-2">New Password</label>
           <input
