4af570d1dcb871664496c102f58592c659b8f0f4
Naryond <giuggey85@gmail.com>
2022-12-13 22:20:07 +0000

    Page title set

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from '../components/Navbar';
 import { AuthContextProvider } from '../context/AuthContext';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import ProtectedRoute from '../components/ProtectedRoute';
 
@@ -12,6 +13,9 @@ function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   return (
     <AuthContextProvider>
+      <Head>
+        <title>Firebase Authenticated Website</title>
+      </Head>
       <NavBar />
       {noAuthRequired.includes(router.pathname) ? (
         <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,9 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Firebase Authenticated Website</title>
-      </Head>
       <main className={styles.main}>
         <h2 className={styles.title}>Firebase Authenticated Website</h2>
         <p className={styles.description}>
